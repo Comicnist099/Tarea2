0273181dc7bec303ae4a147fb4bb279769e985f9
Comicnist099 <marco.ctu@hotmail.com>
2022-01-31 03:26:27 -0600

    Hola

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include('./templates/header.php') ?>
+        <?php echo "Hola ajaj"; ?>
+
+         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+             <ul class="nav navbar-nav">
+                 <li class="nav-item active">
+                     <a class="nav-link" href="#">Nav 1</a>
+                 </li>
+                 <li class="nav-item">
+                     <a class="nav-link" href="#">Nav 2</a>
+                 </li>
+             </ul>
+         </nav>
+<?php include('./templates/footer.php') ?>
